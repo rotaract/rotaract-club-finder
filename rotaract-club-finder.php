b6a8be12d3aa71beae83eedf8da69ec492b4ebaf
Benno Bielmeier <github@bbenno.com>
2021-07-20 22:22:11 +0200

    PHPCBF: Auto-Reformat

--- rotaract-club-finder.php
+++ rotaract-club-finder.php
@@ -6,41 +6,40 @@
  * Version: 1.0
  * Author: Ressort IT-Entwicklung
  * Author: URI: https://rotaract.de
- *
  */
 
 
 function myEnqueueScripts() {
 
-    wp_register_style('rotaract-club-finder', plugins_url( 'rotaract-club-finder.css', __FILE__));
-    wp_enqueue_style('rotaract-club-finder');
+	wp_register_style( 'rotaract-club-finder', plugins_url( 'rotaract-club-finder.css', __FILE__ ) );
+	wp_enqueue_style( 'rotaract-club-finder' );
 
-    wp_enqueue_script('rotaract-club-finder', plugins_url( 'rotaract-club-finder.js', __FILE__));
+	wp_enqueue_script( 'rotaract-club-finder', plugins_url( 'rotaract-club-finder.js', __FILE__ ) );
 
-    $scriptData = array(
-        'clubApiKeyGoogle' => get_option('clubApiKeyGoogle'),
-        'clubApiKeyOpenCage' => get_option('clubApiKeyOpenCage')
-    );
+	$scriptData = array(
+		'clubApiKeyGoogle'   => get_option( 'clubApiKeyGoogle' ),
+		'clubApiKeyOpenCage' => get_option( 'clubApiKeyOpenCage' ),
+	);
 
-    wp_localize_script('rotaract-club-finder', 'scriptData', $scriptData);
+	wp_localize_script( 'rotaract-club-finder', 'scriptData', $scriptData );
 }
 
-add_shortcode('RotaractClubFinder', 'initClubFinder');
+add_shortcode( 'RotaractClubFinder', 'initClubFinder' );
 
 function initClubFinder() {
 
-	$html =  '<b>Suche</b>';
+	$html = '<b>Suche</b>';
 
-	$html .= '<form id="rotaract-club-search">';
-	$html .= '<input type="text" id="rotaract-search" name="search">';
-	$html .= '<select id="club-finder-range">';
-	$html .= '<option value="5">5km</option>';
-	$html .= '<option value="10">10km</option>';
-	$html .= '<option value="20">20km</option>';
-	$html .= '<option value="50">50km</option>';
-	$html .= '</select>';
-       	$html .= '<button type="submit"><i class="fas fa-search" title="Suchen"></i></button>';
-	$html .= '</form>';
+	$html     .= '<form id="rotaract-club-search">';
+	$html     .= '<input type="text" id="rotaract-search" name="search">';
+	$html     .= '<select id="club-finder-range">';
+	$html     .= '<option value="5">5km</option>';
+	$html     .= '<option value="10">10km</option>';
+	$html     .= '<option value="20">20km</option>';
+	$html     .= '<option value="50">50km</option>';
+	$html     .= '</select>';
+		$html .= '<button type="submit"><i class="fas fa-search" title="Suchen"></i></button>';
+	$html     .= '</form>';
 
 	$html .= '<div id="map"></div>';
 	$html .= '<div id="club-finder-list"></div>';
@@ -51,21 +50,21 @@ function initClubFinder() {
 }
 
 // create custom plugin settings menu
-add_action('admin_menu', 'storeLocatorSettings');
+add_action( 'admin_menu', 'storeLocatorSettings' );
 
 function storeLocatorSettings() {
 
-	//create new top-level menu
-	//add_menu_page('Rotaract Store Locator Settings', 'Rotaract Store Locator', 'administrator', __FILE__, 'settingsPage' , plugins_url('/images/icons/club-finder-icon.jpg', __FILE__) );
-	add_menu_page('Rotaract Club Finder Settings', 'Rotaract Club Finder', 'administrator', __FILE__, 'settingsPage' , 'dashicons-location-alt' );
+	// create new top-level menu
+	// add_menu_page('Rotaract Store Locator Settings', 'Rotaract Store Locator', 'administrator', __FILE__, 'settingsPage' , plugins_url('/images/icons/club-finder-icon.jpg', __FILE__) );
+	add_menu_page( 'Rotaract Club Finder Settings', 'Rotaract Club Finder', 'administrator', __FILE__, 'settingsPage', 'dashicons-location-alt' );
 
-	//call register settings function
+	// call register settings function
 	add_action( 'admin_init', 'registerSettingsPage' );
 }
 
 
 function registerSettingsPage() {
-	//register our settings
+	// register our settings
 	register_setting( 'rotaract-club-finder-settings-group', 'clubApiKeyGoogle' );
 	register_setting( 'rotaract-club-finder-settings-group', 'clubApiKeyOpenCage' );
 }
@@ -81,11 +80,11 @@ function settingsPage() {
 			<table class="form-table">
 				<tr valign="top">
 					<th scope="row">Google API-Key</th>
-					<td><input type="text" name="clubApiKeyGoogle" value="<?php echo esc_attr( get_option('clubApiKeyGoogle') ); ?>" /></td>
+					<td><input type="text" name="clubApiKeyGoogle" value="<?php echo esc_attr( get_option( 'clubApiKeyGoogle' ) ); ?>" /></td>
 				</tr>
 				<tr valign="top">
 					<th scope="row">OpenCage API-Key</th>
-					<td><input type="text" name="clubApiKeyOpenCage" value="<?php echo esc_attr( get_option('clubApiKeyOpenCage') ); ?>" /></td>
+					<td><input type="text" name="clubApiKeyOpenCage" value="<?php echo esc_attr( get_option( 'clubApiKeyOpenCage' ) ); ?>" /></td>
 				</tr>
 			</table>
 
--- rotaract-elastic-caller.php
+++ rotaract-elastic-caller.php
@@ -1,10 +1,10 @@
 <?php
 
 $location = '{"lat" : ' . $_REQUEST['lat'] . ', "lon" : ' . $_REQUEST['lng'] . '}';
-if (!empty($_REQUEST['range'])) {
-	$range=$_REQUEST['range'];
+if ( ! empty( $_REQUEST['range'] ) ) {
+	$range = $_REQUEST['range'];
 } else {
-	$range='20';
+	$range = '20';
 }
 
 $searchParam = '{
@@ -27,20 +27,20 @@ $searchParam = '{
         }
 
         }';
-$header = [
-	'content-type: application/json'
-	];
-$url = 'curl -X GET "hosting.rotaract.de:9200/clubs/_search" -H \'Content-Type: application/json\' -d \'' . $searchParam . '\'';
-$url = 'hosting.rotaract.de:9200/clubs/_search';
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl,CURLOPT_HTTPHEADER, $header);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $searchParam);
-$res = curl_exec($curl);
-curl_close($curl);
+$header      = array(
+	'content-type: application/json',
+);
+$url         = 'curl -X GET "hosting.rotaract.de:9200/clubs/_search" -H \'Content-Type: application/json\' -d \'' . $searchParam . '\'';
+$url         = 'hosting.rotaract.de:9200/clubs/_search';
+$curl        = curl_init();
+curl_setopt( $curl, CURLOPT_URL, $url );
+curl_setopt( $curl, CURLOPT_HTTPHEADER, $header );
+curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt( $curl, CURLOPT_POSTFIELDS, $searchParam );
+$res = curl_exec( $curl );
+curl_close( $curl );
 echo $res;
 
 
 
-?>
+
