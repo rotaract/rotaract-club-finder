17d6932ba1f882cbbd1dbce959fecd450163d4c4
Benno Bielmeier <github@bbenno.com>
2021-08-15 13:02:13 +0200

    Refactor elastic caller

--- rotaract-elastic-caller.php
+++ rotaract-elastic-caller.php
@@ -1,42 +1,66 @@
 <?php
 
-$location = '{"lat" : ' . $_REQUEST['lat'] . ', "lon" : ' . $_REQUEST['lng'] . '}';
-if ( ! empty( $_REQUEST['range'] ) ) {
-	$range = $_REQUEST['range'];
-} else {
-	$range = '20';
+/**
+* Receive appointments from elestic that match the search_param.
+*
+* @param String $api_path absolute API path.
+* @param String $search_param API attributes in JSON format.
+*
+* @return array of appointments
+*/
+function elastic_request( string $api_path, string $search_param ): array {
+
+	$url    = trailingslashit( 'hosting.rotaract.de:9200' ) . $api_path;
+	$header = array(
+		'Content-Type' => 'application/json',
+	);
+
+	$res      = wp_remote_post(
+		$url,
+		array(
+			'headers' => $header,
+			'body'    => $search_param,
+		)
+	);
+	$res_body = wp_remote_retrieve_body( $res );
+
+	$result = json_decode( $res_body )->hits->hits;
+	return $result ?: array();
 }
 
-$search_param = '
-{
-	"_source": ["location", "name", "district_name", "homepage_url"],
-	"query" : {
-		"bool" : {
-			"must" : {
-				"match_all" : {}
-			},
-			"filter" : [
-				{
-					"geo_distance" : {
-						"distance" : "' . $range . 'km",
-						"location" : ' . $location . '
-					}
-				},
-				{ "terms": { "status": [ "active", "founding", "preparing" ] } }
-			]
-		}
-	}
-}';
-$header      = array(
-	'content-type: application/json',
-);
-$url         = 'curl -X GET "hosting.rotaract.de:9200/clubs/_search" -H \'Content-Type: application/json\' -d \'' . $search_param . '\'';
-$url         = 'hosting.rotaract.de:9200/clubs/_search';
-$curl        = curl_init();
-curl_setopt( $curl, CURLOPT_URL, $url );
-curl_setopt( $curl, CURLOPT_HTTPHEADER, $header );
-curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt( $curl, CURLOPT_POSTFIELDS, $search_param );
-$res = curl_exec( $curl );
-curl_close( $curl );
-echo $res;
+function get_clubs( string $range, string $lat, string $lng ) {
+
+	$path         = 'clubs/_search';
+	$search_param = array(
+		'_source' => array(
+			'location',
+			'name',
+			'district_name',
+			'homepage_url',
+		),
+		'query' => array(
+			'filter' => array(
+				'geo_distance' => array(
+					'distance' => $range . 'km',
+					'location' => array(
+						'lat' => $lat,
+						'lng' => $lng,
+					),
+				),
+				'terms' => array(
+					'status' => array(
+						'active',
+						'founding',
+						'preparing',
+					)
+				),
+			),
+		),
+	);
+
+	$res = elastic_request( $path, wp_json_encode( $search_param ) );
+	return $res;
+
+}
+
+echo get_clubs( $_REQUEST['range'] ?: '20', $_REQUEST['lat'], $_REQUEST['lng'] );
